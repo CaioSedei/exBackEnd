e54ba2b83e99b205a268b83a2f435b45a9a5cf8f
Caio Sedei <sedeicaio@gmail.com>
2025-09-29 21:11:51 -0300

    aula de testes

--- service/exercicios.js
+++ service/exercicios.js
@@ -1,5 +1,10 @@
 export function somar(num1, num2) {
-    return num1 + num2;
+    const num1Inteiro = num1*100
+    const num2Inteiro = num2*100
+    const result = num1Inteiro + num2Inteiro
+    return result/100
+
+    // return num1 + num2; antes da tratativa de casas decimais
 }
 
 export function salario(valorHr, qtdeHr) {
